4a5e69c89cede9f07a1245accc97d98559d3d9fb
Дмитрий Рогозин <the403studio@yandex.ru>
2026-06-28 14:15:00 +0700

    Refactor login flow and application shell

--- MyAdminExplorer/App.xaml.cs
+++ MyAdminExplorer/App.xaml.cs
@@ -1,18 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using MyAdminExplorer.Infrastructure;
 using System.Windows;
 
 namespace MyAdminExplorer
 {
-    /// <summary>
-    /// Логика взаимодействия для App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            AppPaths.EnsureUsersFile();
+        }
     }
 }
--- MyAdminExplorer/Menu.xaml.cs
+++ MyAdminExplorer/Menu.xaml.cs
@@ -1,56 +1,36 @@
-﻿using MyAdminExplorer.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using MyAdminExplorer.Models;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace MyAdminExplorer
 {
-    /// <summary>
-    /// Логика взаимодействия для Menu.xaml
-    /// </summary>
     public partial class Menu : Window
     {
-        bool _admin;
-        private User _user;
+        private readonly User _user;
+
         public Menu(User user, bool fromAdmin)
         {
-            InitializeComponent();        
-            _admin = fromAdmin;
+            InitializeComponent();
             _user = user;
-            BackAdminPanel.Visibility = fromAdmin ? Visibility.Visible : Visibility.Hidden;
+            BackAdminPanel.Visibility = fromAdmin ? Visibility.Visible : Visibility.Collapsed;
         }
 
         private void GoToExplorer(object sender, RoutedEventArgs e)
         {
-            Explorer explorer = new Explorer(_user.ForbiddenList);
-            explorer.Owner = this;
+            var explorer = new Explorer(_user.ForbiddenList) { Owner = this };
             explorer.Show();
             Hide();
         }
 
         private void Logout(object sender, RoutedEventArgs e)
         {
-            MainWindow main = new MainWindow();
+            var main = new MainWindow();
             main.Show();
             Close();
         }
 
         private void ChangePassword(object sender, RoutedEventArgs e)
         {
-         
-            ChangePass changePass = new ChangePass(_user);
-            changePass.Owner = this;
+            var changePass = new ChangePass(_user) { Owner = this };
             changePass.Show();
             Hide();
         }
@@ -58,7 +38,7 @@ namespace MyAdminExplorer
         private void BackAdmin(object sender, RoutedEventArgs e)
         {
             Owner.Show();
-            Close();            
+            Close();
         }
     }
 }
